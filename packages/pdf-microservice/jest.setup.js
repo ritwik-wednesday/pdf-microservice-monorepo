a3326f6065dab26c919929f1a234135a8ae12eee
ritwik-wednesday <ritwik@wednesday.is>
2022-03-17 22:26:31 +0530

    feat(template): remove tests

--- packages/pdf-microservice/jest.setup.js
+++ packages/pdf-microservice/jest.setup.js
@@ -1,18 +1,6 @@
-import { mockDBClient } from '@utils/testUtils';
-import { DB_ENV } from '@utils/testUtils/mockData';
-
-jest.doMock('@database', () => ({
-  getClient: () => mockDBClient().client,
-  client: mockDBClient().client,
-  connect: () => {}
-}));
-jest.doMock('@database/models', () => ({
-  ...mockDBClient().models
-}));
-
 process.env.ENVIRONMENT_NAME = 'test';
 beforeEach(() => {
-  process.env = { ...process.env, ...DB_ENV, ENVIRONMENT_NAME: 'test' };
+  process.env = { ...process.env, ENVIRONMENT_NAME: 'test' };
 });
 afterEach(() => {
   jest.clearAllMocks();
