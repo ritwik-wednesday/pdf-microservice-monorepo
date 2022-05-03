6c5065e17ff00766ef23f09c8da27207dd913270
ritwik-wednesday <ritwik@wednesday.is>
2022-05-03 09:20:42 +0530

    feat(email): add support to send emails with pdfs attached

---
+++ packages/pdf-microservice/server/services/ses/index.js
@@ -0,0 +1,18 @@
+import nodemailer from 'nodemailer';
+import { defaultProvider } from '@aws-sdk/credential-provider-node';
+const aws = require('@aws-sdk/client-ses');
+
+export const sendSESEmailWithAttachments = mailOptions => {
+  const ses = new aws.SES({
+    apiVersion: '2010-12-01',
+    region: process.env.REGION,
+    defaultProvider
+  });
+
+  // create Nodemailer SES transporter
+  const transporter = nodemailer.createTransport({
+    SES: { ses, aws }
+  });
+
+  return transporter.sendMail(mailOptions);
+};
