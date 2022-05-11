2a657d83d5c6edb9ae08f49975b144839c0ca1e7
ritwik-wednesday <77774719+ritwik-wednesday@users.noreply.github.com>
2022-05-12 00:52:12 +0530

    Revert "fix(cd): fix cd"

--- packages/test-api/server/index.js
+++ packages/test-api/server/index.js
@@ -168,8 +168,8 @@ export const init = async () => {
     ['SIGINT', 'SIGTERM'].forEach(signal => {
       process.on(signal, () => subscriptionServer.close());
     });
-    httpServer.listen(9001, () => {
-      console.log(`Server is now running on http://localhost:9001/`);
+    httpServer.listen(9000, () => {
+      console.log(`Server is now running on http://localhost:9000/graphql`);
     });
     initQueues();
   }
